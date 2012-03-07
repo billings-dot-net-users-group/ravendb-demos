ee34f270c048869844d02e00a7cced2463b1baea
John Clayton <john@codemonkeylabs.com>
2012-03-06 19:41:23 -0700

    Hello World

---
+++ 01 - Hello World/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Document;
+
+namespace _01___Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var documentStore = new DocumentStore { Url = "http://localhost:8080" }.Initialize())
+            {
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(new { Id = "demo/1", Text = "Hello, World!" });
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    dynamic hello = session.Load<dynamic>("demo/1");
+                    Console.WriteLine(hello.Text);
+                }
+            }
+        }
+    }
+}
